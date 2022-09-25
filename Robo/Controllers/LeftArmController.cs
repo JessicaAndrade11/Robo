d918ab7406e616664e5f943b38db9e5e4c292d61
Jessica Andrade <jessicaandrade11@hotmail.com>
2022-09-25 17:24:57 -0300

    arms controllers create

---
+++ Robo/Controllers/LeftArmController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Robo.Domain.Interfaces;
+
+namespace Robo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LeftArmController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeftArmService _leftArmService;
+
+        public LeftArmController(
+            IMapper mapper,
+            ILeftArmService leftArmService)
+        {
+            _mapper = mapper;
+            _leftArmService = leftArmService;
+        }
+    }
+}
---
+++ Robo/Controllers/RightArmController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Robo.Domain.Interfaces;
+
+namespace Robo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RightArmController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IRightArmService _rightArmService;
+
+        public RightArmController(
+            IMapper mapper,
+            IRightArmService rightArmService)
+        {
+            _mapper = mapper;
+            _rightArmService = rightArmService;
+        }
+    }
+}
